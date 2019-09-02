e33414a9578d9a99196e8822c8bd003d8672d582
Min Park <minpark815@gmail.com>
2019-09-02 18:53:59 -0500

    Adds structs

--- sqlinterface.go
+++ sqlinterface.go
@@ -1,5 +1,29 @@
 package sqlinterface
 
+import (
+    "fmt"
+)
+
+// DB : the database struct to store information about the connection
+type DB struct {
+    DbType string
+    Username string
+    Password string
+}
+
+// RowAccess : struct to request rows from the database
+type RowAccess struct {
+    Table string
+    Column string
+    Index int
+}
+
+// InterfaceTest : tests the interface import
 func InterfaceTest() int {
     return 5
+}
+
+// GetRows : fetches rows from DB
+func (rowStruct RowAccess) GetRows() {
+    fmt.Println(rowStruct.Column)
 }
