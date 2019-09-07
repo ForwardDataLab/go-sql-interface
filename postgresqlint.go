721aad09c13b1e5980cfc08a97391f1e5017e81e
Min Park <minpark815@gmail.com>
2019-09-07 15:34:43 -0500

    Removes postgressql

--- postgresqlint.go
+++ postgresqlint.go
@@ -9,23 +9,24 @@ func postgresInitDB(){
 }
 
 func postgresGetRows(db DB, rowAccess RowAccess) []RowStructure {
-    currentDatabase, _ := sql.Open(db.DbType, "user=" + db.Username +
-        " password=" + db.Password +
-        " dbname=" + db.DatabaseName +
-        "sslmode=disable")
-    queryString := "SELECT * FROM " +
-        db.Table +
-        " WHERE " + rowAccess.Column + " = 1"
-    fetchedArr := make([]RowStructure, len(rowAccess.Indices))
-    for index, rowIndex := range rowAccess.Indices {
-        row := currentDatabase.QueryRow(queryString, rowIndex)
-        err := row.Scan(&(fetchedArr[index]).USER_NAME,
-            &(fetchedArr[index]).INDEX_COL)
-        if err != nil {
-            print(err)
-        }
-    }
-    return fetchedArr
+    // currentDatabase, _ := sql.Open(db.DbType, "user=" + db.Username +
+    //     " password=" + db.Password +
+    //     " dbname=" + db.DatabaseName +
+    //     "sslmode=disable")
+    // queryString := "SELECT * FROM " +
+    //     db.Table +
+    //     " WHERE " + rowAccess.Column + " = 1"
+    // fetchedArr := make([]RowStructure, len(rowAccess.Indices))
+    // for index, rowIndex := range rowAccess.Indices {
+    //     row := currentDatabase.QueryRow(queryString, rowIndex)
+    //     err := row.Scan(&(fetchedArr[index]).USER_NAME,
+    //         &(fetchedArr[index]).INDEX_COL)
+    //     if err != nil {
+    //         print(err)
+    //     }
+    // }
+    // return fetchedArr
+    return nil
 }
 
 func postgresInsertRow(db DB, rowStructure RowStructure) int {
