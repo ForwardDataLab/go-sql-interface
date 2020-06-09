26b4096843f108c13f7f299bf056789e6f0d1fa9
Thomas Mao <haoyum2@illinois.edu>
2020-06-09 18:40:54 -0500

    fix millisecond bug

--- test.go
+++ test.go
@@ -1,43 +1,43 @@
 package sqlinterface
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 // InterfaceTest : tests the interface import
 func InterfaceTest() int {
-    return 5
+	return 5
 }
 
 // InterfaceBatchTimeTest : tests the batch interface get rows times
 func InterfaceBatchTimeTest(db DB, ra RowAccess) {
-    fmt.Println("Starting batch test")
-    totalDuration := int64(0)
-    numIterations := 10
+	fmt.Println("Starting batch test")
+	totalDuration := int64(0)
+	numIterations := 10
 
-    for i := 0; i < numIterations; i ++ {
-        start := time.Now()
-        db.GetRowsBatch(ra)
-        currentTime := time.Since(start)
-        totalDuration += currentTime.Milliseconds()
-        fmt.Println(currentTime)
-    }
-    fmt.Println("total average time is: ", float64(totalDuration) / float64(numIterations))
+	for i := 0; i < numIterations; i++ {
+		start := time.Now()
+		db.GetRowsBatch(ra)
+		currentTime := time.Since(start)
+		totalDuration += currentTime / 10 * *6
+		fmt.Println(currentTime)
+	}
+	fmt.Println("total average time is: ", float64(totalDuration)/float64(numIterations))
 }
 
 // InterfaceSerialTimeTest : tests the serial interface get rows times
 func InterfaceSerialTimeTest(db DB, ra RowAccess) {
-    fmt.Println("Starting Serial test")
-    totalDuration := int64(0)
-    numIterations := 10
+	fmt.Println("Starting Serial test")
+	totalDuration := int64(0)
+	numIterations := 10
 
-    for i := 0; i < numIterations; i ++ {
-        start := time.Now()
-        db.GetRowsSerial(ra)
-        currentTime := time.Since(start)
-        totalDuration += currentTime.Milliseconds()
-        fmt.Println(currentTime)
-    }
-    fmt.Println("total average time is: ", float64(totalDuration) / float64(numIterations))
+	for i := 0; i < numIterations; i++ {
+		start := time.Now()
+		db.GetRowsSerial(ra)
+		currentTime := time.Since(start)
+		totalDuration += currentTime / 10 * *6
+		fmt.Println(currentTime)
+	}
+	fmt.Println("total average time is: ", float64(totalDuration)/float64(numIterations))
 }
